2900b685c80feff65bc305074336118e0eb06f02
Joe di Stefano <joeydi@okaypl.us>
2014-04-22 18:35:36 -0400

    add billboard update script

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,8 +1,26 @@
-//@codekit-prepend "svg-support.js"
-//@codekit-prepend "swfobject.js"
-
 if (/Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor)) {
     document.getElementById('recommended_format').innerHTML = 'AAC+';
-} else if (swfobject.hasFlashPlayerVersion("1")) {
+} else if (typeof swfobject !== 'undefined' && swfobject.hasFlashPlayerVersion("1")) {
     document.getElementById('recommended_format').innerHTML = 'Flash';
 }
+
+var VPR = VPR || {};
+
+VPR.update_billboard = function () {
+    $.get('/billboard', function (data) {
+        var billboard = $('#billboard'),
+            new_billboard = $(data);
+
+        billboard.replaceWith(new_billboard.hide().fadeIn());
+    });
+}
+
+VPR.init_billboard = function () {
+    var my_interval = window.setInterval(function () {
+        VPR.update_billboard();
+    }, 5 * 60 * 1000);
+}
+
+$(document).ready(function () {
+    VPR.init_billboard();
+});
