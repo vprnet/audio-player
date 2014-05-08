00b7b62538e5c80475439c66d827b6c591ab396d
Matt Parrilla <matthew.parrilla@gmail.com>
2014-05-08 10:39:34 -0400

    Remove index from view

--- app/views.py
+++ app/views.py
@@ -7,27 +7,27 @@ SHEET_ID = 'tzE2PsqJoWRpENlMr-ZlS8A'
 #SHEET_ID = 'tIk5itVcfOHUmakkmpjCcxw'  # Demo sheet
 
 
-@app.route('/')
-def index():
-    page_url = BASE_URL + request.path
-    page_title = 'Audio Player'
-    stream_name = "My Place"
-
-    social = {
-        'title': "VPR Audio Player",
-        'subtitle': "",
-        'img': "static/img/logo/vpr-logo-share.jpg",
-        'description': "Listen to the live streams of VPR News, VPR Classical, the BBC, Jazz24 and My Place.",
-        'twitter_text': "News, Classical, the BBC and more. The VPR Audio Player:",
-        'twitter_hashtag': ""
-    }
-
-    return render_template('content.html',
-        page_title=page_title,
-        social=social,
-        stream_name=stream_name,
-        page_url=page_url)
-
+#@app.route('/')
+#def index():
+#    page_url = BASE_URL + request.path
+#    page_title = 'Audio Player'
+#    stream_name = "My Place"
+#
+#    social = {
+#        'title': "VPR Audio Player",
+#        'subtitle': "",
+#        'img': "static/img/logo/vpr-logo-share.jpg",
+#        'description': "Listen to the live streams of VPR News, VPR Classical, the BBC, Jazz24 and My Place.",
+#        'twitter_text': "News, Classical, the BBC and more. The VPR Audio Player:",
+#        'twitter_hashtag': ""
+#    }
+#
+#    return render_template('content.html',
+#        page_title=page_title,
+#        social=social,
+#        stream_name=stream_name,
+#        page_url=page_url)
+#
 
 @app.route('/billboard')
 def billboard():
